b06ceef3ea1ee8cacb89bc9e15c70db4238e227f
ricky <patrickchinwafor@gmail.com>
2025-12-28 15:30:56 +0100

    feat: Render initial Post component UI with subreddit details.

--- components/post/Post.tsx
+++ components/post/Post.tsx
@@ -12,7 +12,43 @@ async function Post({ post, userId }: PostProps) {
     const votes = await getPostVotes(post._id);
     const vote = await getUserPostVoteStatus(post._id, userId);
     const comments = await getPostComments(post._id, userId);
-    return <div>Post</div>;
+    return (
+        <article
+            key={post._id}
+            className="relative bg-white rounded-md shadow-sm border border-gray-200 hover:border-gray-300 transition-colors"
+        >
+            <div className="flex ">
+                {/* Vote Buttons */}
+                {/* <PostVoteButtons
+                    contentId={post._id}
+                    votes={votes}
+                    vote={vote}
+                    contentType="post"
+                /> */}
+
+                {/* Post Content */}
+                <div className="flex-1 p-3">
+                    <div className="flex items-center gap-2 text-xs text-gray-500 mb-2">
+                        {post.subreddit && (
+                            <>
+                                <a
+                                    href={`/community/${post.subreddit.slug}`}
+                                    className="font-medium hover:underline"
+                                >
+                                    c/{post.subreddit.title}
+                                </a>
+                                <span>.</span>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
+
+            {/* Buttons */}
+            {/* Report Button */}
+            {/* Delete Button */}
+        </article>
+    );
 }
 
 export default Post;
